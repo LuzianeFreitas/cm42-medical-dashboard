a338bd4875fce015129a6476f1ccbe55b3e2e613
Luziane Freitas <freitas.lu@outlook.com>
2022-03-29 21:26:36 -0300

    add - home page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,10 +1,12 @@
 import { Routes ,Route } from "react-router-dom";
 
 import Patient from './pages/Patient';
+import Home from "./pages/Home";
 
 const RoutesMedical = (): JSX.Element => {
   return (
       <Routes>
+        <Route path="/" element={<Home/>} />
         <Route path="/patient/:id" element={<Patient/>} />
       </Routes>
   );
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from "./styles"; 
+const Home = (): JSX.Element => {
+    return(
+        <Container>
+            <h3>
+                Dashboard
+            </h3>
+        </Container>
+    );
+};
+
+export default Home;
