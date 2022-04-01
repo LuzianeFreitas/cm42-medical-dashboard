0e54ff983711f082d18d6d8ca63438692b5d3536
Luziane Freitas <freitas.lu@outlook.com>
2022-03-31 23:10:17 -0300

    added test to sideBar component

--- src/components/SideBar/SideBar.spec.tsx
+++ src/components/SideBar/SideBar.spec.tsx
@@ -1,6 +1,14 @@
-import { render } from '@testing-library/react'
+import { render,screen, fireEvent } from '@testing-library/react'
+import {createMemoryHistory} from 'history'
 import { ReactNode } from 'react'
 import SideBar from '.'
+import { useSchedule } from '../../hooks/useSchedule';
+
+import { Router } from "react-router";
+import { createBrowserHistory } from "history";
+
+
+
 
 jest.mock('react-router-dom', () => {
     return {
@@ -8,15 +16,52 @@ jest.mock('react-router-dom', () => {
     }
 })
 
+const patients = [
+    {
+        id: 1,
+        name: 'Luziane Teste 1',
+        document: '78998745624',
+        healthSystemId: '5689472569',
+        birthday: '1998-02-17T15:00:00Z',
+        insurancePlan: 'International',
+      },
+      {
+        id: 2,
+        name: 'Luziane Teste 2',
+        document: '78998745624',
+        healthSystemId: '5689472569',
+        birthday: '1998-02-17T15:00:00Z',
+        insurancePlan: 'International',
+      }
+]
+
+const mockeduseScheduleHook = useSchedule as jest.Mock;
+jest.mock('../../hooks/useSchedule');
+
 
 describe('SideBar component', () => {
-    it('should be able to render corret', () => {
-        const { getByText } = render(
+    beforeEach(() => {
+        mockeduseScheduleHook.mockReturnValue({
+          patients: patients
+        })
+    });
+
+    it('should be able to render correct', () => {
+        render(
             <SideBar/>
         )
 
-        expect(getByText('Medical Test')).toBeInTheDocument;
-        expect(getByText('Select a Patient')).toBeInTheDocument;
+        expect(screen.getByText('Medical Test')).toBeInTheDocument;
+        expect(screen.getByText('Select a Patient')).toBeInTheDocument;
+
+    });
+
+    it('should be able to display a list of patients', () => {
+        render(
+            <SideBar/>
+        )
         
+        expect(screen.getByText('Luziane Teste 1')).toBeInTheDocument;
+        expect(screen.getByText('Luziane Teste 2')).toBeInTheDocument;
     });
 });
